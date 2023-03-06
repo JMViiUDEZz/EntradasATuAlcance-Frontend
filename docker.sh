5c5ec2acf2d008c5c761af860efd69988b6d800a
José María Viúdez <jmviudezp2003@gmail.com>
2023-03-06 13:31:41 +0100

    update Windows

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 # Comandos Docker
-docker compose -f var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml build
-docker compose -f var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml down
-docker compose -f var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml up
+docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml build
+docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml down
+docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml up
