aceebb443ef7dc2d260b1e0a075223d9ca484790
José María Viúdez <jmviudezp2003@gmail.com>
2023-03-12 22:39:38 +0100

    another commit

--- api/EntradasATuAlcanceApi.ts
+++ api/EntradasATuAlcanceApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const entradasATuAlcanceApi = axios.create({
-    baseURL: 'http://localhost:3000/api'
-    // baseURL: 'http://194.61.28.54:82/api'
+    // baseURL: 'http://localhost:3000/api'
+    baseURL: 'http://194.61.28.54:80/api'
 
 });
 export default entradasATuAlcanceApi
