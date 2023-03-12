01fa8c43cf09765f1003ed86c8bf85ecfb31539a
José María Viúdez <jmviudezp2003@gmail.com>
2023-03-12 22:26:56 +0100

    another commit

--- docker.sh
+++ docker.sh
@@ -1,4 +1,7 @@
 # Comandos Docker
-docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml build --no-cache
-docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml down --remove-orphans
-docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml up
+# docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml build --no-cache
+# docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml down --remove-orphans
+# docker compose -f /var/lib/jenkins/workspace/Job-EntradasATuAlcance-Frontend/docker-compose.yml up
+
+docker build -t app .
+docker run -p 81:80 --name app-container app-image
